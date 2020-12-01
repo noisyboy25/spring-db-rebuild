c56714af708fd08c45123914599bcb8f85ecf577
noisyboy25 <dj402and@gmail.com>
2020-12-01 10:51:50 +0300

    first commit

---
+++ src/main/java/com/example/springdbrebuild/SpringDbRebuildApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdbrebuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbRebuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDbRebuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdbrebuild/SpringDbRebuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdbrebuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDbRebuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
