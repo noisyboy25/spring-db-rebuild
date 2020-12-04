7f14f137009f77e8297e0d9b5cefafc5bbc1653f
noisyboy25 <dj402and@gmail.com>
2020-12-04 21:36:02 +0300

    Disable default security

--- src/main/java/com/example/springdbrebuild/SpringDbRebuildApplication.java
+++ src/main/java/com/example/springdbrebuild/SpringDbRebuildApplication.java
@@ -2,8 +2,9 @@ package com.example.springdbrebuild;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SpringDbRebuildApplication {
 
 	public static void main(String[] args) {
